2bcf018b96bad1cb992568f6efd1fd3d3c93de16
David Viviano <viviano.d@husky.neu.edu>
2019-11-05 16:16:59 -0500

    Migrate Code to new Repo

--- visualization.js
+++ visualization.js
@@ -1,1 +1,51 @@
-console.log('hello world!');
+// Parse survey data
+d3.csv("/data/survey-data.csv").then(function (data) {
+    tabulate(data, data.columns)
+});
+
+/**
+ * Create D3 table and populate with survey data
+ * 
+ * @param {Object} data 
+ * @param {Object} columns 
+ */
+function tabulate(data, columns) {
+    let svg = d3.select("#vis-svg");
+    let table = svg.append("foreignObject")
+        .attr("width", 800)
+        .attr("height", '100%')
+        .append("xhtml:body");
+    let thead = table.append("thead");
+    let tbody = table.append("tbody");
+
+    thead.append('tr')
+        .selectAll('th')
+        .data(columns)
+        .enter()
+        .append('th')
+        .attr("style", "font-size:12px",)
+        .text(function (column) {
+            return column
+        });
+
+    let rows = tbody.selectAll('tr')
+        .data(data)
+        .enter()
+        .append('tr');
+
+    let cells = rows.selectAll('td')
+        .data(function (row) {
+            return columns.map(function (column) {
+                return {column: column, value: row[column]}
+            })
+        })
+        .enter()
+        .append('td')
+        .style('text-align', 'center')
+        .style('font-size', '12px')
+        .html(function (d) {
+            return d.value;
+        });
+
+    return table;
+}
